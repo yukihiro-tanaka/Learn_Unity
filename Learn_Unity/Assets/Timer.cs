e4ce50d06d369595392b9a1f5f25adc053ae201b
田中幸浩 <>
2020-06-21 23:58:44 +0900

    タイマーをUIで表示するプロジェクトを新規作成しました。

---
+++ Learn_Unity/Assets/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private float countTime = 10.0f;
+    public bool isTimeUp { private set; get; } = false;
+
+    // Update is called once per frame
+    void Update () {
+        if (countTime > 0) {
+            countTime -= Time.deltaTime;
+            if (countTime <= 0) {
+                // Adjust extend time
+                countTime = 0.0f;
+                OnTimeUp();
+            }
+            GetComponent<Text>().text = countTime.ToString("F2");
+        }
+    }
+
+    void OnTimeUp () {
+        isTimeUp = true;
+    }
+}
---
+++ Learn_Unity/Assets/ToTitleButtonControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToTitleButtonControl : MonoBehaviour
+{
+    [SerializeField] private Timer timer = default;
+    [SerializeField] private GameObject toTitleButton = default;
+
+    // Update is called once per frame
+    void Update () {
+        if (timer.isTimeUp) {
+            toTitleButton.SetActive(true);
+        }
+    }
+}
